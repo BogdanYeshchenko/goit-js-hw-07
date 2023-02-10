a274250545c5ef94b58011199c224c39f7130418
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-02-10 19:29:23 +0200

    add home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const el = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryItemsHtml = galleryItems
+  .map(
+    (el) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" data-source="${el.original}" alt="${el.description}"/></a></div>`
+  )
+  .join("");
+
+el.gallery.innerHTML = galleryItemsHtml;
+
+el.gallery.addEventListener("click", hanleGalleryModalWithImage);
+
+const hanleGalleryModalWithImage = (event) => {
+  event.preventDefault();
+
+  const isClickOnImg = event.target.classList.contains("gallery__image");
+
+  if (!isClickOnImg) {
+    return;
+  }
+
+  const imgSource = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${imgSource}" width="800" height="600">`);
+
+  instance.show();
+
+  el.gallery.addEventListener("keydown", (ev) => {
+    if (ev.code !== "Escape") {
+      return;
+    }
+    console.log(ev.code);
+
+    instance.close();
+  });
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const imageGalleryHtml = galleryItems
+  .map(
+    (el) =>
+      `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+  </a>`
+  )
+  .join("");
+
+galleryEl.innerHTML = imageGalleryHtml;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
